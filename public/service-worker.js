d16eee628d9ea8825ededb7f0d5103d2026bc219
Matthew Williams <matthew.lloyd.williams@gmail.com>
2020-03-30 22:48:18 +0100

    Install service worker

---
+++ public/service-worker.js
@@ -0,0 +1,18 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
+
+if (workbox) {
+  const { registerRoute } = workbox.routing;
+  const { CacheFirst } = workbox.strategies;
+  const { CacheableResponsePlugin } = workbox.cacheableResponse;
+
+  registerRoute(
+    /\.(?:css|html|jpg)$/,
+    new CacheFirst({
+      plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })]
+    })
+  );
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
